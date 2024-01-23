eb6deb5d5454db5c2e5a7940a7178018cc406581
cjbellmyer <cjbellmyer5984@gmail.com>
2024-01-23 13:08:18 -0600

    admin page check

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import AdminProjectType from "./AdminProjectType";
 
 const Admin = () => {
-    return (
-        <div>
-            <h1>Admin</h1>
-            <AdminProjectType type="Bathrooms" />
-            <AdminProjectType type="Kitchens" />
-            <AdminProjectType type="Decks" />
-        </div>
-    );
+    if (typeof window !== "undefined") {
+        // Your client-side code that uses window goes here
+        return (
+            <div>
+                <h1>Admin</h1>
+                <AdminProjectType type="Bathrooms" />
+                <AdminProjectType type="Kitchens" />
+                <AdminProjectType type="Decks" />
+            </div>
+        );
+    }
 };
 
 export default Admin;
