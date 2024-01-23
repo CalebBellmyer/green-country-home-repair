fdaa01c16286871b29219b0b6d82762ea404e230
cjbellmyer <cjbellmyer5984@gmail.com>
2024-01-23 13:14:03 -0600

    undid admin check

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import AdminProjectType from "./AdminProjectType";
 
 const Admin = () => {
-    if (typeof window !== "undefined") {
-        // Your client-side code that uses window goes here
-        return (
-            <div>
-                <h1>Admin</h1>
-                <AdminProjectType type="Bathrooms" />
-                <AdminProjectType type="Kitchens" />
-                <AdminProjectType type="Decks" />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>Admin</h1>
+            <AdminProjectType type="Bathrooms" />
+            <AdminProjectType type="Kitchens" />
+            <AdminProjectType type="Decks" />
+        </div>
+    );
 };
 
 export default Admin;
