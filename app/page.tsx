7c234457ccbe5d3151996ce5a96d5ac81eeaa2b3
cjbellmyer <cjbellmyer5984@gmail.com>
2024-04-21 15:27:41 -0500

    deebug

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <main className="flex flex-col min-h-screen w-full">
             <Header />
-            <Hero />
+            {/* <Hero /> */}
 
             {/* <ProjectType type="Kitchens" />
             <ProjectType type="Bathrooms" />
