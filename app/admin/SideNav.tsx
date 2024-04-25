6e901643650648e56eff31e68dbed509e7eea554
cjbellmyer <cjbellmyer5984@gmail.com>
2024-04-25 14:13:44 -0500

    removed nav border

--- app/admin/SideNav.tsx
+++ app/admin/SideNav.tsx
@@ -13,7 +13,7 @@ export default function SideNav({ setType }: SideNavProps) {
                 {/* Replace with your company logo */}
             </div>
             <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-primary rounded-md  pt-24">
-                <div className="mb-2 flex flex-col h-20 items-center justify-start rounded-md bg-primary p-4 md:h-40  border-2 border-black">
+                <div className="mb-2 flex flex-col h-20 items-center justify-start rounded-md bg-primary p-4 md:h-40  ">
                     {/* <Logo /> */}
                     <AdminNavButton type="Bathrooms" setType={setType} />
                     <AdminNavButton type="Kitchens" setType={setType} />
