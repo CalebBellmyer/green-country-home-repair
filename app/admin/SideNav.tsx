3c8fd7c6ea6f1c1ce34bbcd6aea2d4e9b6cd40ce
cjbellmyer <cjbellmyer5984@gmail.com>
2024-04-29 10:16:27 -0500

    fixed auth states

--- app/admin/SideNav.tsx
+++ app/admin/SideNav.tsx
@@ -1,22 +1,35 @@
+import logout from "@/pages/api/logout";
 import AdminNavButton from "./AdminNavButton";
 import Link from "next/link";
 
 type SideNavProps = {
     setType: (type: string) => void;
 };
+const handleLogout = () => {
+    logout();
+    console.log("logout");
+};
 
 export default function SideNav({ setType }: SideNavProps) {
     return (
-        <div className="lex h-full flex-col px-3 py-4 md:px-2 bg-primary rounded-md ">
+        <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-primary rounded-md ">
             <div className="text-lg font-bold text-white text-center">
                 <Link href="/">Green Country</Link>{" "}
                 {/* Replace with your company logo */}
             </div>
             <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-primary rounded-md  pt-24">
-                <div className="mb-2 flex flex-col h-20 items-center justify-start rounded-md bg-primary p-4 md:h-40  ">
+                <div className="mb-2 flex flex-col  items-center justify-start rounded-md bg-primary p-4   ">
                     <AdminNavButton type="Bathrooms" setType={setType} />
                     <AdminNavButton type="Kitchens" setType={setType} />
                     <AdminNavButton type="Decks" setType={setType} />
+                </div>
+                <div>
+                    <button
+                        onClick={handleLogout}
+                        className="text-white bg-red-500 p-2 rounded-md w-full"
+                    >
+                        Logout
+                    </button>
                 </div>
             </div>
         </div>
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,24 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import SideNav from "./SideNav";
 import ProjectTypeAdmin from "./projectTypeAdmin";
 
 const Page = () => {
     const [currentType, setCurrentType] = useState<string>("Bathrooms");
+    const router = useRouter();
+    const auth = getAuth();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                router.push("/login");
+            }
+        });
+
+        return () => unsubscribe();
+    }, [auth, router]);
 
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-gray-100">
