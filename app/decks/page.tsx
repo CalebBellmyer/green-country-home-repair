3a808b6a878b39e501d7badcb53fc90cdf0e6064
cjbellmyer <cjbellmyer5984@gmail.com>
2024-05-05 13:41:42 -0500

    fixed decks page layout

--- app/decks/page.tsx
+++ app/decks/page.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import ProjectDisplay from "../_components/ProjectDisplay";
 
 const Page = () => {
-    return (
-        <div className="min-h-screen bg-gray-100 flex justify-center items-center py-8">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-                <ProjectDisplay type="Decks" />
-            </div>
-        </div>
-    );
+    return <ProjectDisplay type="Decks" />;
 };
 export default Page;
