8204760af038f92f4ec3c5d0657105c6a630a803
cjbellmyer <cjbellmyer5984@gmail.com>
2024-05-06 12:41:23 -0500

    added hero skeleton

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 const Hero = () => {
     const [bgImageUrl, setBgImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch("/api/Hero")
@@ -12,6 +13,7 @@ const Hero = () => {
             .then((data) => {
                 if (data.imageUrl) {
                     setBgImageUrl(data.imageUrl);
+                    setLoading(false);
                 }
             })
             .catch((error) => console.error("Error fetching images:", error));
@@ -20,7 +22,7 @@ const Hero = () => {
     return (
         <div className="flex justify-center w-full">
             <div className="relative flex justify-center overflow-hidden w-full 2xl:w-5/6 h-[16rem] sm:h-[24rem] md:h-[28rem] lg:h-[36rem]">
-                {bgImageUrl && (
+                {!loading ? (
                     <Image
                         src={bgImageUrl}
                         alt="Hero"
@@ -28,7 +30,12 @@ const Hero = () => {
                         style={{ objectFit: "cover" }}
                         placeholder="empty"
                         quality={100}
+                        priority
                     />
+                ) : (
+                    <div className="animate-pulse duration-100 rounded-lg min-width-[640px] w-full h-full bg-gray-300 ">
+                        {" "}
+                    </div>
                 )}
             </div>
         </div>
