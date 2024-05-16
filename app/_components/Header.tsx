bae648a2f0a3470f8c275625ac57649995a60cb2
cjbellmyer <cjbellmyer5984@gmail.com>
2024-05-16 16:51:36 -0500

    added company logo

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,21 +1,24 @@
-"use client";
-import React, { useState } from "react";
-import Link from "next/link";
-import HamburgerMenu from "./HamburgerMenu";
+'use client';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import HamburgerMenu from './HamburgerMenu';
+import Logo from './Logo';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <header className="bg-primary z-50">
-            <nav className="container mx-auto flex justify-between items-center py-4 px-6">
-                <div className="text-lg font-bold text-white">
-                    <Link href="/">Green Country</Link>{" "}
+        <header className='bg-primary z-50'>
+            <nav className='container mx-auto flex justify-between items-center py-2 px-6'>
+                <div className='text-lg font-bold text-white'>
+                    <Link href='/'>
+                        <Logo />
+                    </Link>{' '}
                     {/* Replace with your company logo */}
                 </div>
 
                 {/* Hamburger Menu Icon for Mobile */}
-                <div className="md:hidden">
+                <div className='md:hidden'>
                     <HamburgerMenu
                         setIsMenuOpen={setIsMenuOpen}
                         isMenuOpen={isMenuOpen}
@@ -25,45 +28,45 @@ const Header = () => {
                 {/* Navigation Links */}
                 <ul
                     className={`${
-                        isMenuOpen ? "flex" : "hidden"
-                    } flex-col md:flex md:flex-row md:space-x-4 absolute md:relative top-20 md:top-auto left-0 md:left-auto w-full md:w-auto bg-primary md:bg-transparent p-6 md:p-0 transition-all duration-300 ease-in-out z-10`}
+                        isMenuOpen ? 'flex' : 'hidden'
+                    } flex-col md:flex md:flex-row md:space-x-4 absolute md:relative top-16 md:top-auto left-0 md:left-auto w-full md:w-auto bg-primary md:bg-transparent p-6 md:p-0 transition-all duration-300 ease-in-out z-10`}
                 >
                     <li>
                         <Link
-                            href="/kitchens"
-                            className="text-white hover:text-gray-200"
+                            href='/kitchens'
+                            className='text-white hover:text-gray-200'
                         >
                             Kitchens
                         </Link>
                     </li>
                     <li>
                         <Link
-                            href="/bathrooms"
-                            className="text-white hover:text-gray-200"
+                            href='/bathrooms'
+                            className='text-white hover:text-gray-200'
                         >
                             Bathrooms
                         </Link>
                     </li>
                     <li>
                         <Link
-                            href="/decks"
-                            className="text-white hover:text-gray-200"
+                            href='/decks'
+                            className='text-white hover:text-gray-200'
                         >
                             Decks
                         </Link>
                     </li>
                     <li>
                         <Link
-                            href="/misc"
-                            className="text-white hover:text-gray-200"
+                            href='/misc'
+                            className='text-white hover:text-gray-200'
                         >
                             Misc
                         </Link>
                     </li>
                     <li>
                         <Link
-                            href="/contact"
-                            className="text-white hover:text-gray-200"
+                            href='/contact'
+                            className='text-white hover:text-gray-200'
                         >
                             Contact
                         </Link>
---
+++ app/_components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Logo() {
+    return (
+        <img
+            src='/decal.png'
+            alt="Green Country Home Repair's logo"
+            className=' w-20  z-50 cursor-pointer'
+        />
+    );
+}
+
+export default Logo;
