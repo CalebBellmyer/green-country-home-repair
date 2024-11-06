ffe5192b038dba776e198818a0c723faab94c84f
Caleb <cjbellmyer5984@gmail.com>
2024-11-06 14:04:40 -0600

    started about section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "./_components/Header";
 import Hero from "./_components/Hero";
 import ProjectType from "./_components/ProjectType";
 import Footer from "./_components/Footer";
+import AboutSection from "./_components/AboutSection";
 
 export default function Home() {
     const maxImages = 6;
@@ -11,6 +12,9 @@ export default function Home() {
             <Hero />
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-6 mb-6">
                 <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
+                    <div className="col-span-1 md:col-span-3 lg:col-span-3">
+                        <AboutSection/>
+                    </div>
                     <div className="col-span-1 md:col-span-3 lg:col-span-3">
                         <ProjectType type="Kitchens" maxImages={maxImages} />
                     </div>
