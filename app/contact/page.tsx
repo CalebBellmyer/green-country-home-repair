846a72c2f99d4411098f418db2b9d32c12cb0be1
Caleb <cjbellmyer5984@gmail.com>
2024-11-07 13:15:03 -0600

    start contact info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,6 +7,11 @@ function Contact() {
     return (
         <div>
             <Header />
+            <div className="flex items-center justify-center p-4 bg-gray-100">
+                <span className="text-lg md:text-2xl font-semibold hover:text-slate-500 transition duration-300 ">
+                    Contact Us
+                </span> 
+            </div>
             <ContactForm />
             <div className="fixed inset-x-0 bottom-0">
                 <Footer />
