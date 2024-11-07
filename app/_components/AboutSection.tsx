f6b0b62b09d6521e0cefe3637c815bd20272f2bf
Caleb <cjbellmyer5984@gmail.com>
2024-11-07 13:13:18 -0600

    styled about us text

--- app/_components/AboutSection.tsx
+++ app/_components/AboutSection.tsx
@@ -8,24 +8,24 @@ const AboutSection = () => {
       </span>
 
       <div className="w-full max-w-4xl mx-auto">
-        <p>
+        <p className='text-lg py-2'>
           Green Country Home Repair LLC was started in Bartlesville in 2015 by brothers Johnny and
           Patrick Bellmyer. With a goal of doing quality home repairs and exceeding customer’s
           expectations our small company has thrived and grown. In 2023, Jose Ojeda joined as a
           partner bringing with him a long history of quality construction and a wealth of knowledge.
         </p>
-        <p>
+        <p className='text-lg py-2'>
           We now have a team of craftsmen and top quality sub-contractors that offer everything
           from small repairs to building custom homes. We have become known for our custom
           decks and outdoor entertainment areas, over the top kitchens, and bathrooms that make
           you not want to leave them.
         </p>
-        <p>
+        <p className='text-lg py-2'>
           We take pride in our work and keep a clean work site. Many customers have commented
           that they were very pleased with our work but were also very happy with the cleanliness we
           kept the work area (their home)!
         </p>
-        <p>
+        <p className='text-lg py-2'>
           We typically book projects well in advance so call today for a free estimate and get your
           project scheduled!
         </p>
